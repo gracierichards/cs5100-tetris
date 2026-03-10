95070aacd0611f6d3cf570e58b7d5699f2fcd348
Gracie Richards <gracier@gmail.com>
2026-03-10 03:21:22 -0400

    Counts number of pieces dropped, a rudimentary progress measure

--- main.py
+++ main.py
@@ -7,20 +7,23 @@ from gym_tetris.actions import MOVEMENT
 env = gym_tetris.make('TetrisA-v0')
 env = JoypadSpace(env, MOVEMENT)
 
-instructions = [6, 3, 1, 6, 3, 2]
+# instructions = [6, 3, 1, 6, 3, 2]
+
+# Get the total number of pieces dropped. Takes in info[statistics]
+def total_pieces(statistics):
+  total = 0
+  for type0 in statistics:
+    total += statistics[type0]
+  return total
+
 
 done = True
 state = env.reset()
 for step in range(10**4):
-  if step % 3 == 0:
-    action = 6
-  elif step % 3 == 1:
-    action = 3
-  else:
-    action = 1
-  state, reward, done, info = env.step(action)
+  state, reward, done, info = env.step(env.action_space.sample())
   env.render()
   if done:
+    print("Total pieces dropped:", total_pieces(info["statistics"]))
     state = env.reset()
     break
 
